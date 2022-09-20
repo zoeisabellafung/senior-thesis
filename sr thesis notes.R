eb9d4d8dcceec982848a25fc80cad8637b882697
zoeisabellafung <funggzoee@gmail.com>
2022-09-20 11:34:31 -0700

    refine post-fire data set & begin loading leaf traits

--- sr thesis notes.R
+++ sr thesis notes.R
@@ -3,4 +3,9 @@
 
 # to undo a commit: in terminal, type "git reset HEAD~"
 
-# how to add latitude & longitude columns to usda 1 prefire data?
+# data processed so far:
+  # USDA RDA 1981-2016 fire, pre-fire data
+  # USDA RDA 2003-2017 fire data
+
+# leaf traits to consider:
+  # leaf area, leaf area per dry mass (SLA), leaf length (excluding petiole), leaf water content per dry mass, leaf litter decomposition rate constant
